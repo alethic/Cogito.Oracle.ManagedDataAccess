df8e024e2d17d2a49a2a44e7aafe081777b03d97
Jerome Haltom <wasabi@cogito.cx>
2017-05-05 10:55:43 -0500

    Passing IEnumerable instead of dict. Organize as name/value pairs internally. Add index version of indexer. Add test project.

---
+++ OracleObjectTypeTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Oracle.ManagedDataAccess.Client;
+
+namespace Oracle.ManagedDataAccess.Extensions.Tests
+{
+
+    [TestClass]
+    public class OracleObjectTypeTests
+    {
+
+        [TestMethod]
+        public void Test_basic_type_def()
+        {
+            var t = new OracleObjectType("TEST", "TYPE", new OracleObjectTypeAttribute[]
+            {
+                new OracleObjectTypeAttribute("Attr1", null, "VARCHAR", OracleDbType.Varchar2, 20, null, null),
+                new OracleObjectTypeAttribute("Attr2", null, "VARCHAR", OracleDbType.Varchar2, 20, null, null),
+                new OracleObjectTypeAttribute("Attr3", null, "VARCHAR", OracleDbType.Varchar2, 20, null, null)
+            });
+
+            Assert.IsTrue(t.Attributes.Count == 3);
+            Assert.IsTrue(t.Attributes[2].Name == "Attr3" && t.Attributes[2].TypeName == "VARCHAR");
+        }
+
+    }
+
+}
