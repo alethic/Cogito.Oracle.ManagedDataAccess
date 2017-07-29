339e45ca29104cb2cfe8de59fcee38c09da4afeb
Jerome Haltom <wasabi@cogito.cx>
2017-07-29 12:05:02 -0500

    Implemented VarChar2 support in XML serializer for Oracle. Implemented some test cases around serialization and deserialization.

--- OracleObjectTypeTests.cs
+++ OracleObjectTypeTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Xml.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using Oracle.ManagedDataAccess.Client;
@@ -34,14 +35,42 @@ namespace Oracle.ManagedDataAccess.Extensions.Tests
         /// Tests whether we can serialize the given object type.
         /// </summary>
         [TestMethod]
-        public void Test_serialization()
+        public void Test_xml_serialization()
         {
             var o = TestOracleObjectType.CreateValue();
             o["Attr1"] = "value1";
             o["Attr2"] = "value2";
             o["Attr3"] = "value3";
-            o["Attr4"] = Guid.NewGuid().ToByteArray();
+            o["Attr4"] = new Guid("BD971DD2-7CD1-459E-B901-08705EC71A17").ToByteArray();
             var x = OracleObjectXmlTransferSerializer.Serialize(o);
+
+            var t = XDocument.Parse(@"
+                <TYPE>
+                  <Attr1>value1</Attr1>
+                  <Attr2>value2</Attr2>
+                  <Attr3>value3</Attr3>
+                  <Attr4>D21D97BDD17C9E45B90108705EC71A17</Attr4>
+                </TYPE>");
+
+            Assert.IsTrue(XNode.DeepEquals(t, x));
+        }
+
+        [TestMethod]
+        public void Test_xml_deserialization()
+        {
+            var t = XDocument.Parse(@"
+                <TYPE>
+                  <Attr1>value1</Attr1>
+                  <Attr2>value2</Attr2>
+                  <Attr3>value3</Attr3>
+                  <Attr4>D21D97BDD17C9E45B90108705EC71A17</Attr4>
+                </TYPE>");
+            var x = OracleObjectXmlTransferSerializer.Deserialize(TestOracleObjectType, t);
+
+            Assert.IsTrue((string)x["Attr1"] == "value1");
+            Assert.IsTrue((string)x["Attr2"] == "value2");
+            Assert.IsTrue((string)x["Attr3"] == "value3");
+            Assert.IsTrue(new Guid((byte[])x["Attr4"]) == new Guid("BD971DD2-7CD1-459E-B901-08705EC71A17"));
         }
 
     }
